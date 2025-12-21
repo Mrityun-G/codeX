c9b99eec2bcf8ae38e14aa227584d9451aba51e1
MRITYUNJAI G <msg2gmanikandan@gmail.com>
2025-12-21 14:19:46 +0530

    This is the basic view of login page of codeX

--- app/src/main/java/com/example/codex/MainActivity.kt
+++ app/src/main/java/com/example/codex/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.gestures.snapping.SnapPosition.Center
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.TextAutoSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,8 +29,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,15 +45,34 @@ class MainActivity : ComponentActivity() {
             val painter = painterResource(id = R.drawable.mountain)
             val description = "LOGIN PAGE"
             val title = "LOGIN PAGE"
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(10.dp)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding()
+            ) {
                 ImageCard(
                     painter = painter,
                     contentDescription = description,
                     title = title
                 )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Black.copy(alpha = 0.2f)
+                )
+                {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+
+
+                    ) {
+                        Button(onClick = { /*TODO*/ })
+                        {
+                            Text(text = "Login Now")
+                        }
+                    }
 
                 }
             }
@@ -53,44 +80,52 @@ class MainActivity : ComponentActivity() {
     }
 
 
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
+    @Composable
+    fun ImageCard(
+        painter: Painter,
+        contentDescription: String,
+        title: String,
+        modifier: Modifier = Modifier
     ) {
-        Box(
-            modifier = Modifier
-                .height(1000.dp)
-                .fillMaxWidth())
-            {
+        Card(
+            modifier = modifier.fillMaxWidth(),
+        ) {
 
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-
-            )
-            Box(modifier = Modifier
-                .padding(25.dp),
-                contentAlignment = Alignment.BottomCenter
+            Box(
+                modifier = Modifier
+                    .height(1000.dp)
+                    .fillMaxWidth()
             )
             {
-                Text(title, style = TextStyle(
-                    color = Color.White,
-                    textAlign = TextAlign.Center,
-                    fontWeight = androidx.compose.ui.text.font.FontWeight.SemiBold,
-                    fontSize = 22.sp))
+
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop
+
+                )
+                Box(
+                    modifier = Modifier
+                        .padding(25.dp),
+                    contentAlignment = Alignment.BottomCenter
+                )
+                {
+                    Text(
+                        title, style = TextStyle(
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 22.sp
+                        )
+                    )
+                }
+
             }
 
         }
 
     }
-
 }
+
+
 
