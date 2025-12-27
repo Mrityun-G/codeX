53f6b9a83f1b7410a4474447b00c3d4e6bd5aa0c
MRITYUNJAI G <msg2gmanikandan@gmail.com>
2025-12-27 09:37:01 +0530

    Button

--- app/src/main/java/com/example/codex/MainActivity.kt
+++ app/src/main/java/com/example/codex/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.codex
 
+import android.hardware.camera2.params.BlackLevelPattern
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,20 +12,28 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
@@ -36,6 +45,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.codex.ui.theme.Purple40
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,13 +78,25 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center
 
 
-                    ) {
+                    )
+                    {
                         Button(onClick = { /*TODO*/ })
                         {
-                            Text(text = "Login Now")
+                            Text(text = "Login",color = Color.LightGray)
+
+                        }
+                        ElevatedButton(onClick = { /*TODO*/ }) {
+                            Text(text = "Login with Google",color = Color.Black)
+
+                        }
+                        TextButton(
+                            modifier = Modifier.padding(10.dp),
+                            onClick = { /*TODO*/ }
+                        ) {
+                            Text(text = "Learn more",color = Color.White)
+
                         }
                     }
-
                 }
             }
         }
