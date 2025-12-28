77265ca6a486e78718f195dcaa138026f5afa6da
MRITYUNJAI G <msg2gmanikandan@gmail.com>
2025-12-28 12:36:23 +0530

    Buttons

--- app/src/main/java/com/example/codex/MainActivity.kt
+++ app/src/main/java/com/example/codex/MainActivity.kt
@@ -2,13 +2,12 @@ package com.example.codex
 
 import android.hardware.camera2.params.BlackLevelPattern
 import android.os.Bundle
+import android.widget.NumberPicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.VectorConverter
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.snapping.SnapPosition
-import androidx.compose.foundation.gestures.snapping.SnapPosition.Center
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,6 +19,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -29,8 +29,12 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
@@ -45,8 +49,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.codex.ui.theme.Purple40
-import org.w3c.dom.Text
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,21 +83,22 @@ class MainActivity : ComponentActivity() {
 
 
                     )
+
                     {
                         Button(onClick = { /*TODO*/ })
                         {
-                            Text(text = "Login",color = Color.LightGray)
+                            Text(text = "Login Now",color= Color.White)
 
                         }
                         ElevatedButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Login with Google",color = Color.Black)
+                            Text(text = "Login with Google",color= Color.Black)
 
                         }
                         TextButton(
                             modifier = Modifier.padding(10.dp),
                             onClick = { /*TODO*/ }
                         ) {
-                            Text(text = "Learn more",color = Color.White)
+                            Text(text = "Learn more",color= Color.White)
 
                         }
                     }
@@ -134,7 +139,8 @@ class MainActivity : ComponentActivity() {
                 )
                 {
                     Text(
-                        title, style = TextStyle(
+                        title, style = TextStyle
+                            (
                             color = Color.White,
                             textAlign = TextAlign.Center,
                             fontWeight = FontWeight.SemiBold,
@@ -142,11 +148,8 @@ class MainActivity : ComponentActivity() {
                         )
                     )
                 }
-
             }
-
         }
-
     }
 }
 
