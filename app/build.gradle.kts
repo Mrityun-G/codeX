d48ab4d892d15761bf902c4bd83244bedf359d7b
MRITYUNJAI G <msg2gmanikandan@gmail.com>
2025-12-29 14:15:28 +0530

    This is the basic view of login page of codeX and view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3.1.1.1)
     implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/codex/MainActivity.kt
+++ app/src/main/java/com/example/codex/MainActivity.kt
@@ -1,163 +1,51 @@
-package com.example.codex
+package com.example.yourappname
 
-import android.hardware.camera2.params.BlackLevelPattern
-import android.os.Bundle
-import android.widget.NumberPicker
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.VectorConverter
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.materialIcon
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.VerticalAlignmentLine
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Composable
+fun TextFieldView() {
+    val textState = remember { mutableStateOf("") }
+    val focusColor = remember { mutableStateOf(Color.Gray) }
 
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            val painter = painterResource(id = R.drawable.mountain)
-            val description = "LOGIN PAGE"
-            val title = "LOGIN PAGE"
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .padding()
-            ) {
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title
-                )
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Black.copy(alpha = 0.2f)
-                )
-                {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-
-
-                    )
-
-                    {
-                        Button(onClick = { /*TODO*/ })
-                        {
-                            Text(text = "Login Now",color= Color.White)
-
-                        }
-                        ElevatedButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Login with Google",color= Color.Black)
-
-                        }
-                        TextButton(
-                            modifier = Modifier.padding(10.dp),
-                            onClick = { /*TODO*/ }
-                        ) {
-                            Text(text = "Learn more",color= Color.White)
-
-                        }
-                        OutlinedButton(
-                            modifier = Modifier.padding(10.dp),
-                            onClick = { /*TODO*/ }
-                        ) {
-                            Text(text = "Sign Up",color= Color.White)
-
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-
-    @Composable
-    fun ImageCard(
-        painter: Painter,
-        contentDescription: String,
-        title: String,
-        modifier: Modifier = Modifier
-    ) {
-        Card(
-            modifier = modifier.fillMaxWidth(),
-        ) {
-
-            Box(
-                modifier = Modifier
-                    .height(1000.dp)
-                    .fillMaxWidth()
+    TextField(
+        value = textState.value,
+        onValueChange = { newText ->
+            textState.value = newText
+        },
+        modifier = Modifier
+            .height(70.dp)
+            .border(
+                width = 3.dp,
+                color = focusColor.value,
+                shape = RoundedCornerShape(8.dp)
             )
-            {
-
-                Image(
-                    painter = painter,
-                    contentDescription = contentDescription,
-                    contentScale = ContentScale.Crop
-
-                )
-                Box(
-                    modifier = Modifier
-                        .padding(25.dp),
-                    contentAlignment = Alignment.BottomCenter
-                )
-                {
-                    Text(
-                        title, style = TextStyle(
-                            color = Color.White,
-                            textAlign = TextAlign.Center,
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 25.sp
-                        )
-                    )
-                }
-            }
-        }
-    }
+            .onFocusChanged {
+                focusColor.value = if (it.isFocused) Color.Blue else Color.Gray
+            },
+        textStyle = TextStyle(
+            color = Color.Black,
+            fontSize = 20.sp
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Done
+        ),
+        placeholder = { Text("Enter email") }
+    )
 }
-
-
-
